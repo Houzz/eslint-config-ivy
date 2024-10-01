4d26fb3d302cfd8b7ae9a7c019341a6d1067bb36
Yes Lee <yes.lee@houzz.com>
2024-10-01 15:59:37 +0800

    chore: Add .prettierrc.js to format eslint config file

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// Copied from ivy-shared-components
+// https://github.com/Houzz/ivy-shared-components/blob/master/.prettierrc
+
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+};
